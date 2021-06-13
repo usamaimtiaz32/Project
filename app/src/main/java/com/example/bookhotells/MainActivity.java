6bb202b9a136be3c3ccbcabde1733ae620b3abe7
usama <bsef18@43>
2021-06-13 23:51:16 +0500

    project

--- app/src/main/java/com/example/bookhotells/MainActivity.java
+++ app/src/main/java/com/example/bookhotells/MainActivity.java
@@ -1,21 +1,55 @@
 package com.example.bookhotells;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView username, password;
+    Button btnlogin;
+    DBHelper DB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    public void Search(View view) {
-        Intent intent=new Intent(MainActivity.this, Search.class);
-        startActivity(intent);
+        username = (TextView) findViewById(R.id.username1);
+        password = (TextView) findViewById(R.id.password1);
+        btnlogin = (Button) findViewById(R.id.btnsignin1);
+        DB = new DBHelper(this);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+
+                if(user.equals("")||pass.equals(""))
+                    Toast.makeText(MainActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+                else{
+                    Boolean checkuserpass = DB.checkusernamepassword(user, pass);
+                    if(checkuserpass==true){
+                        Toast.makeText(MainActivity.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
+                        Intent intent  = new Intent(getApplicationContext(), Search.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(MainActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 
+
+
+
+    public void SignIn(View view) {
+        Intent intent=new Intent(MainActivity.this, SignIn.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/bookhotells/signUp.java
+++
@@ -1,4 +0,0 @@
-package com.example.bookhotells;
-
-public class signUp {
-}
